ea90f3ecc95e1d0d63e5bbc9ae279e44d7248061
ThatGuyWill <williamjackson982@gmail.com>
2021-01-05 11:19:04 -0800

    Updates

---
+++ precache-manifest.4f9956ba5d3b1d9883af7f0746fcb2d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "386785fb26ab085bb2d1737f2c93de93",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "909ecab93b9bbd4238db",
+    "url": "/twitter-clone/static/css/main.5bbf8ba9.chunk.css"
+  },
+  {
+    "revision": "a75d0b9e154a169c630d",
+    "url": "/twitter-clone/static/js/2.112c3b80.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/twitter-clone/static/js/2.112c3b80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "909ecab93b9bbd4238db",
+    "url": "/twitter-clone/static/js/main.10b70846.chunk.js"
+  },
+  {
+    "revision": "4ea1c8ca6c4e7890228d",
+    "url": "/twitter-clone/static/js/runtime-main.a74d6e56.js"
+  }
+]);
